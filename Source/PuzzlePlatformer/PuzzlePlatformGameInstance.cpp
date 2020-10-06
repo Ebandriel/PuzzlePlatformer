536584a044eeb25966cc1fe2215b57e2607cb49d
Ebandriel <67708633+Ebandriel@users.noreply.github.com>
2020-10-06 12:56:05 +0100

    Added Menu System

--- Source/PuzzlePlatformer/PuzzlePlatformGameInstance.cpp
+++ Source/PuzzlePlatformer/PuzzlePlatformGameInstance.cpp
@@ -1,31 +1,99 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "PuzzlePlatformerGameInstance.h"
 
-#include "PuzzlePlatformGameInstance.h"
+#include "Engine/Engine.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
 
-UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer)
+#include "PlatformTrigger.h"
+#include "MenuSystem/MainMenu.h"
+#include "MenuSystem/MenuWidget.h"
+
+UPuzzlePlatformerGameInstance::UPuzzlePlatformerGameInstance(const FObjectInitializer& ObjectInitializer)
 {
-	UE_LOG(LogTemp, Warning, TEXT("GameInstance constructor"));
+	ConstructorHelpers::FClassFinder<UUserWidget> MenuBPClass(TEXT("/Game/MenuSystem/WBP_MainMenu"));
+	if (!ensure(MenuBPClass.Class != nullptr)) return;
+
+	MenuClass = MenuBPClass.Class;
+
+	ConstructorHelpers::FClassFinder<UUserWidget> InGameMenuBPClass(TEXT("/Game/MenuSystem/WBP_InGameMenu"));
+	if (!ensure(InGameMenuBPClass.Class != nullptr)) return;
+
+	InGameMenuClass = InGameMenuBPClass.Class;
 }
 
-void UPuzzlePlatformGameInstance::Init()
+void UPuzzlePlatformerGameInstance::Init()
 {
-	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+	UE_LOG(LogTemp, Warning, TEXT("Found class %s"), *MenuClass->GetName());
 }
 
-void UPuzzlePlatformGameInstance::Host()
+void UPuzzlePlatformerGameInstance::LoadMenu()
 {
+	if (!ensure(MenuClass != nullptr)) return;
+
+	Menu = CreateWidget<UMainMenu>(this, MenuClass);
+	if (!ensure(Menu != nullptr)) return;
+
+	Menu->Setup();
+
+	Menu->SetMenuInterface(this);
+}
+
+void UPuzzlePlatformerGameInstance::InGameLoadMenu()
+{
+	if (!ensure(InGameMenuClass != nullptr)) return;
+
+	UMenuWidget* Menu = CreateWidget<UMenuWidget>(this, InGameMenuClass);
+	if (!ensure(Menu != nullptr)) return;
+
+	Menu->Setup();
+
+	Menu->SetMenuInterface(this);
+}
+
+
+void UPuzzlePlatformerGameInstance::Host()
+{
+	if (Menu != nullptr)
+	{
+		Menu->Teardown();
+	}
+
 	UEngine* Engine = GetEngine();
-	if (!ensure(Engine != nullptr))return;
+	if (!ensure(Engine != nullptr)) return;
+
 	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
 	UWorld* World = GetWorld();
-	if (!ensure(World != nullptr))return;
+	if (!ensure(World != nullptr)) return;
+
 	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
-void UPuzzlePlatformGameInstance::Join(FString Address)
+void UPuzzlePlatformerGameInstance::Join(const FString& Address)
 {
+	if (Menu != nullptr)
+	{
+		Menu->Teardown();
+	}
+
 	UEngine* Engine = GetEngine();
-	if (!ensure(Engine != nullptr)) { return; }
-	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"),*Address));
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
 }
+
+void UPuzzlePlatformerGameInstance::LoadMainMenu()
+{
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel("/Game/MenuSystem/MainMenu", ETravelType::TRAVEL_Absolute);
+}
+
--- Source/PuzzlePlatformer/PuzzlePlatformGameInstance.h
+++ Source/PuzzlePlatformer/PuzzlePlatformGameInstance.h
@@ -4,21 +4,41 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
-#include "PuzzlePlatformGameInstance.generated.h"
+#include "MenuSystem/MenuInterface.h"
+#include "PuzzlePlatformerGameInstance.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
-class PUZZLEPLATFORMER_API UPuzzlePlatformGameInstance : public UGameInstance
+class PUZZLEPLATFORMER_API UPuzzlePlatformerGameInstance : public UGameInstance, public IMenuInterface
 {
 	GENERATED_BODY()
+
 public:
-	UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer);
+	UPuzzlePlatformerGameInstance(const FObjectInitializer& ObjectInitializer);
+
 	virtual void Init();
 
+	UFUNCTION(BlueprintCallable)
+		void LoadMenu();
+
+	UFUNCTION(BlueprintCallable)
+		void InGameLoadMenu();
+
 	UFUNCTION(Exec)
-	void Host();
+		void Host() override;
+
 	UFUNCTION(Exec)
-	void Join(FString Address);
+		void Join(const FString& Address) override;
+
+	virtual void LoadMainMenu() override;
+
+private:
+	TSubclassOf<class UUserWidget> MenuClass;
+	TSubclassOf<class UUserWidget> InGameMenuClass;
+
+	class UMainMenu* Menu;
+
 };
+
