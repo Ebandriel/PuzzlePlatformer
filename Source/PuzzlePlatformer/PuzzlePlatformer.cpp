3716915cad045f8a0190e40e68571b09731cb426
Ebandriel <67708633+Ebandriel@users.noreply.github.com>
2020-10-06 14:24:46 +0100

    cleanup update

--- Source/PuzzlePlatformer/PuzzlePlatformer.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "PuzzlePlatformer.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PuzzlePlatformer, "PuzzlePlatformer" );
- 
